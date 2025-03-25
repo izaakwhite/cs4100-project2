b5d1904e035d1267fdeff85880860db00e363543
Izaak White <ww655421@ohio.edu>
2025-03-24 22:33:28 -0400

    New New shiii, got that token parsing <token name> nonsense you feel cuh

--- runscript.sh
+++ runscript.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# Izaak White and Joshua Moner
 
 flex tokenizer.l
 
-g++ lex.yy.c -o scanner
+g++ lex.yy.c -o scanner -ll
 
 ./scanner < plisTest.txt > output.txt
 
+./scanner < plisTest.txt
+
+rm -f lex.yy.c
+
+rm -f scanner
