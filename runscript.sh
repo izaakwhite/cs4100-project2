d1b8a49d58f20995a8e66b4558fb3046934c4fb6
Izaak White <ww655421@ohio.edu>
2025-03-24 22:40:08 -0400

    added another lexxer thats the same as the first just with no %s meaning tokens with no data next to it

--- runscript.sh
+++ runscript.sh
@@ -5,9 +5,21 @@ flex tokenizer.l
 
 g++ lex.yy.c -o scanner -ll
 
-./scanner < plisTest.txt > output.txt
+./scanner < plisTest.txt > tokens_with_values.txt
 
 ./scanner < plisTest.txt
+
+rm -f lex.yy.c
+
+rm -f scanner
+
+flex tokenizer_no_yytext.l
+
+g++ lex.yy.c -o scanner -ll
+
+./scanner < plisTest.txt > tokens.txt
+
+# diff output.txt output2.txt > diff.txt
 
 rm -f lex.yy.c
 
